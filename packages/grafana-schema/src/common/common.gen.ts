b3ae64317a44f8b2801651d995aa43986936861a
Marcelino Gonzalez <marcelino.gonzalez@arcelormittal.com>
2024-03-06 20:53:07 +0100

    fix: recreated schema + errors fixed

--- packages/grafana-schema/src/common/common.gen.ts
+++ packages/grafana-schema/src/common/common.gen.ts
@@ -146,6 +146,7 @@ export interface MapLayerOptions {
 export enum FrameGeometrySourceMode {
   Auto = 'auto',
   Coords = 'coords',
+  GeoJSON = 'geojson',
   Geohash = 'geohash',
   Lookup = 'lookup',
 }
@@ -873,6 +874,7 @@ export interface FrameGeometrySource {
    * Field mappings
    */
   geohash?: string;
+  geojson?: string;
   latitude?: string;
   longitude?: string;
   lookup?: string;
