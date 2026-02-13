af19110dcb257d086b28809729b5fc7ed841b11d
Marcelino Gonzalez <marcelino.gonzalez@arcelormittal.com>
2026-02-13 12:43:02 +0100

    fix: check if window.caches exists before accessing it

--- public/app/AppWrapper.tsx
+++ public/app/AppWrapper.tsx
@@ -60,10 +60,12 @@ export class AppWrapper extends Component<AppWrapperProps, AppWrapperState> {
     $('.preloader').remove();
 
     // clear any old icon caches
-    const cacheKeys = await window.caches.keys();
-    for (const key of cacheKeys) {
-      if (key.startsWith('grafana-icon-cache') && key !== this.iconCacheID) {
-        window.caches.delete(key);
+    if (window.caches) {
+      const cacheKeys = await window.caches.keys();
+      for (const key of cacheKeys) {
+        if (key.startsWith('grafana-icon-cache') && key !== this.iconCacheID) {
+          window.caches.delete(key);
+        }
       }
     }
   }
